b01688ddec85bf89dba17b897ba4bec6e7dc6447
GodGefest <godgefest@gmail.com>
2013-04-03 20:32:54 +0400

    count of arguments and buffersize checking. /n -> debuginfo

--- dupme/main.c
+++ dupme/main.c
@@ -3,7 +3,13 @@
 
 int main(int argc, char** argv) {
     int k;
+    if (argc < 2) {
+        return 1;
+    }
     k = atoi(argv[1]);
-    printf("k = %d", k);
+    if (k < 1) {
+        return 1;
+    }
+    printf("k = %d\n", k);
     return 0;
 }
