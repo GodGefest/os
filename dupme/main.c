7115d75c6e05cdaf76ce73aa96ca552b96c50012
GodGefest <godgefest@gmail.com>
2013-04-03 21:06:33 +0400

    defines. buffer init. basic read func

--- dupme/main.c
+++ dupme/main.c
@@ -1,8 +1,16 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define STDIN 0
+#define STDOUT 1
 
 int main(int argc, char** argv) {
-    int k;
+    int k = 0;
+    char* buffer = 0;
+    int read_return = 0;
+    int cur_busy = 0;
+    int eof = 0;
     if (argc < 2) {
         return 1;
     }
@@ -10,6 +18,19 @@ int main(int argc, char** argv) {
     if (k < 1) {
         return 1;
     }
-    printf("k = %d\n", k);
+    buffer = malloc(k);
+                                    printf("k = %d\n", k);
+    while (1) {
+        read_return = read(STDIN, buffer + cur_busy, k - cur_busy); 
+                                    printf("read_return = %d\n", read_return);
+        if (read_return = 0) {
+            eof = 1;
+        } else if (read_return < 0) {
+            return 1;
+        }
+        if (eof) {
+           break;
+        } 
+    }
     return 0;
 }
