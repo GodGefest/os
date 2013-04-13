4a3af0d8ffc1c39ed28615adc736f172dab18431
GodGefest <godgefest@gmail.com>
2013-04-13 05:33:46 +0400

    without normal print

--- filter/filter.c
+++ filter/filter.c
@@ -15,33 +15,17 @@ int find_del(char d, char* buf, int count) {
     return -1;
 }
 
-int run_cmd_on(char* buffer, char* argv[], int argc, int from, int to) {
+int run_cmd_on(char* _argv[], int to, int j) {
     int i;
-    char* _argv[argc - optind + 1];
-    int j = 0;
-   // int STDOUTcp = dup(1);
+    int STDOUTcp = dup(1);
     int devnull = open("/dev/null", O_WRONLY);
-    while (argv[j + optind][0] != '{' || argv[j + optind][1] != '}' || argv[j + optind][2] != 0) {
-        j++;
-    } 
-    memcpy(_argv, argv + optind, sizeof(char * ) * j);
-    char * tmp = malloc(to - from + 1);
-    for (i = 0; i < to - from; i++) {
-        tmp[i] = buffer[from + i];
-    }
-    tmp[to] = 0;
-    _argv[j] = tmp;
-    if (optind + j + 1 < argc) {
-        memcpy(_argv + j + 1, argv + optind + j + 1, argc - optind - j - 1); 
-    }
-    _argv[argc - optind] = NULL;
     int pid;
     int stat_val;
     if(pid = fork()) {
         wait(&stat_val);
         if (WIFEXITED(stat_val) && (WEXITSTATUS(stat_val) == 0)) {
     //        dup2(STDOUTcp, 1);
-            write(1, tmp, to);
+            write(1, _argv[j], to);
         }
         close(devnull);
     } else {
@@ -75,13 +59,22 @@ int main(int argc, char* argv[])
                 break; 
         }
     }
-
     char* buffer = malloc(buf_size);
     int len = 0; 
     int read_res = 0;
+    int j = 0;
     int i = 0;
     int eof = 0;
-    int from;
+    char** _argv = malloc(sizeof(char*) * (argc - optind + 1));
+    while (argv[j + optind][0] != '{' || argv[j + optind][1] != '}' || argv[j + optind][2] != 0) {
+        j++;
+    } 
+    memcpy(_argv, argv + optind, sizeof(char * ) * j);
+    if (optind + j + 1 < argc) {
+        memcpy(_argv + j + 1, argv + optind + j + 1, argc - optind - j - 1); 
+    }
+    _argv[argc - optind] = NULL;
+    
     while(!eof) {
         read_res = read(STDIN, buffer + len, buf_size - len);
         //EOF - ?
@@ -89,14 +82,16 @@ int main(int argc, char* argv[])
             eof = 1;
         }
         int d_pos = find_del(flag, buffer + len, read_res);
-        from = d_pos;
         len = len + read_res;
         if (d_pos != -1) {
             while (d_pos != -1) { 
-                run_cmd_on(buffer, argv, argc, 0, d_pos);
+                char * tmp = malloc(sizeof(char) * (d_pos + 1));
+                memcpy(tmp, buffer, d_pos);
+                _argv[j] = tmp;
+                run_cmd_on(_argv, d_pos, j);
+                free(tmp);
                 memmove(buffer, buffer + d_pos + 1, len - d_pos - 1);
                 len = len - d_pos - 1;
-                from = 0;
                 d_pos = find_del(flag, buffer, len);
             }
         }
@@ -106,7 +101,13 @@ int main(int argc, char* argv[])
             return -3;
         }
         buffer[len + 1] = flag;
-        run_cmd_on(buffer, argv, argc, 0, len + 1);
+        char * tmp = malloc(len + 1 - 0);
+        memcpy(tmp, buffer, len + 1);
+        _argv[j] = tmp;
+        run_cmd_on(_argv, len + 1, j);
+        free(tmp);
     }
+    free(_argv);
+    free(buffer);
     return 0;
 }
