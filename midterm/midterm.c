4388c56eafda3eb85dd20b7fdb1119f7a15e0b93
GodGefest <godgefest@gmail.com>
2013-04-22 21:47:07 +0400

    status after in class working

--- midterm/midterm.c
+++ midterm/midterm.c
@@ -36,47 +36,86 @@ int _strlen(char* str) {
     return len; 
 }
 
-void find_range(char* str, int* from, int* to) {
+int find_places(char* str, int where[]) {
     int len = _strlen(str); 
-    char* tmp = malloc (len);
     int i = 0;
-    while (str[i] != '-') {
+    int count = 0;
+    while (i < len - 1) {
+        if (str[i] == '{' && str[i + 1] == '}') {
+            where[count++] = i;
+        }
         ++i;
     }
-    memmove(tmp, str, i);
-    tmp[i] = 0;
-    *from = atoi(tmp);
-    memmove(tmp, str + i + 1, len - i - 1);
-    tmp[len - i - 1] = 0;
-    *to = atoi(tmp); 
-    free(tmp);
+    return count;
 } 
 
 // Return value: 0 - ok, 1 - not ok;
-int check_for_range(char* str, int len, int from, int to) {
+int check_for_result(char* str, int len, char* cmd, int where[], int where_size) {
+    if (len == 0) {
+        len = 1;
+        str[0] = 0;
+        str[1] = 0;
+        str[2] = 0;
+    }
+    const int max_cmd_len = 2048;
+    char* tmp = malloc(max_cmd_len);
     int i;
-    if (str[0] == '-') {
-        i = 1;
-    } else {
-        i = 0;
+    where[where_size] = _strlen(cmd);
+    memcpy(tmp, cmd, where[0]);
+    int cur_pos = where[0];
+    for (i = 0; i < where_size; i++) {
+        memcpy(tmp + cur_pos, str, len);
+        cur_pos += len;
+        memcpy(tmp + cur_pos, cmd + where[i] + 2, where[i + 1] - where[i] - 2);
+        cur_pos += where[i + 1] - where[i] - 2; 
     }
-    for (; i < len; ++i) {
-        if (str[i] < '0' || str[i] > '9') {
-            return 1;
+    tmp[cur_pos] = 0;
+
+    char** _tmp = malloc(1000);
+    i = 0;
+    int space_pos = find_del(' ', tmp, cur_pos);
+    while ((space_pos = find_del(' ', tmp, cur_pos)) != -1) {
+        _tmp[i] = malloc(space_pos + 1);
+        memcpy(_tmp[i], tmp, space_pos);
+        _tmp[i][space_pos] = 0;
+        memmove(tmp, tmp + space_pos + 1, cur_pos -= space_pos);  
+        i++;
+    } 
+    _tmp[i] = malloc(cur_pos + 1);
+    memcpy(_tmp[i], tmp, cur_pos);
+    _tmp[i][cur_pos] = 0;
+    _tmp[i + 1] = NULL;
+    int devnull = open("/dev/null", O_WRONLY);
+    int pid;
+    int stat_val;
+    int ans = 1;
+    if (pid = fork()) {
+        wait(&stat_val);
+        if (WIFEXITED(stat_val) && (WEXITSTATUS(stat_val) == 0)) {
+           ans = 0;
         }
+        close(devnull);
+    } else {
+        dup2(devnull, 1);
+        execvp(_tmp[0], _tmp);
+        exit(1);
     }
-    char* tmp = malloc(len + 1);
-    memmove(tmp, str, len);
-    tmp[len] = 0;
-    int ans = atoi(tmp);
+    i = 0;
+    while (_tmp[i] != NULL) {
+        free(_tmp[i]);
+        ++i;
+    }
+    free(_tmp);
     free(tmp);
-    if (ans < from || ans > to) {
+    if (ans) {
         return 1;
+    } else {
+        return 0;
     }
-    return 0;
 }
 
-void eat_string(char* str, int len, int position, int from, int to) { 
+void eat_string(char* str, int len, int position, char* cmd, 
+        int where[], int where_size) { 
     int count = 1;
     int begin = 0; // begin - first symbol of token, end - first symbol after.
     int end = 0;
@@ -87,13 +126,16 @@ void eat_string(char* str, int len, int position, int from, int to) {
         ++begin;
     }
     if (begin != len) {
-        end = begin + 1;
+        end = begin;
         while (str[end] != ' ' && end < len) {
             ++end;    
         }    
-        if (!check_for_range(str + begin, end - begin, from, to)) {
+        char* temp = malloc(_strlen(str));    
+        memcpy(temp, str, len);
+        if (!check_for_result(temp + begin, end - begin, cmd, where, where_size)) {
             print(STDOUT, str, len);
         }
+        free(temp);
     }
 }
 
@@ -104,9 +146,8 @@ int main(int argc, char* argv[])
     }
 
     int position = atoi(argv[1]);
-    int from;
-    int to;
-    find_range(argv[2], &from, &to);
+    int where[100];
+    int where_size = find_places(argv[2], where);
 
 
     const char del = '\n';
@@ -126,7 +167,7 @@ int main(int argc, char* argv[])
         len = len + read_res;
         if (d_pos != -1) {
             while (d_pos != -1) { 
-                eat_string(buffer, d_pos, position, from, to);
+                eat_string(buffer, d_pos, position, argv[2], where, where_size);
                 memmove(buffer, buffer + d_pos + 1, sizeof(char) * (len - d_pos - 1));
                 len = len - d_pos - 1;
                 d_pos = find_del(del, buffer, len);
