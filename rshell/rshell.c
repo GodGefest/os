ce6343dcb3e3e27b8f2742b2ff5213b07d278534
GodGefest <godgefest@gmail.com>
2013-05-24 16:43:35 +0400

    Initial commit

---
+++ rshell/rshell.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
