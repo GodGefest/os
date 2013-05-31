a540ee87530a5fe2432a0d3501235d3803d4df39
GodGefest <godgefest@gmail.com>
2013-05-31 20:07:10 +0400

    new version. dont work

--- rshell/rshell.c
+++ rshell/rshell.c
@@ -7,12 +7,118 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <pty.h>
+#include <poll.h>
 
 #define STDIN 0
 #define STDOUT 1
 #define STDERR 2
 
 #define MAX_COUNT_OF_CONNECTIONS 5
+
+struct FD_pair 
+{
+    int fds[2];
+    bool deads[2];
+    bool closed;
+
+    FD_pair(int in, int out)
+    {
+        fds[0] = in;
+        fds[1] = out;
+        deads[0] = false;
+        deads[1] = false;
+        closed = false;
+    }
+
+    bool both_dead()
+    {
+        return (!deads[0] && !deads[1]);
+    }
+
+};
+
+void my_poll(int fd1, int fd2)
+{ 
+    int buf_size = 4096;
+    char buf1to2[buf_size];
+    int len1to2 = 0;
+    char buf2to1[buf_size];
+    int len2to1 = 0;
+    int readed = 0;
+    int written = 0;
+    FD_pair fdp[2] = { FD_pair(fd1, fd2), FD_pair(fd2, fd1) };
+    struct pollfd fds[2];
+    fds[0].fd = fd1;
+    fds[1].fd = fd2;
+    fds[0].events = POLLIN;
+    fds[1].events = POLLIN;
+    while (!fdp[0].both_dead() && !fdp[1].both_dead()) 
+    {
+        int ret = poll(fds, 2, -1);
+        if (ret > 0) 
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                if ((fds[i].revents & POLLERR) || 
+                        (fds[i].revents & POLLHUP))
+                {
+                    fdp[i ^ 1].deads[i] = true;
+                    fdp[i].deads[i ^ 1] = true;
+                    fdp[0].closed = true;
+                    fdp[1].closed = true;
+                }
+            }
+            if ((!fdp[0].deads[0]) && (len1to2 < buf_size) 
+                    && (fds[0].revents & POLLIN) && (!fdp[0].closed))
+            {
+                readed = read(fd1, buf1to2 + len1to2, buf_size - len1to2);
+                len1to2 += readed;
+                if (readed == 0)
+                {
+                    fdp[0].deads[0] = true; 
+                    fdp[0].closed = true;
+                }
+            }
+            if ((!fdp[1].deads[1]) && (len2to1 > 0) && (fds[0].revents & POLLOUT))
+            {
+                written = write(fd1, buf2to1, len2to1);
+                memmove(buf2to1, buf2to1 + written, len2to1 - written);
+                len2to1 -= written;
+            }
+            if ((!fdp[1].deads[0]) && (len2to1 < buf_size) 
+                    && (fds[1].revents & POLLIN) && (!fdp[0].closed))
+            {
+                readed = read(fd2, buf2to1 + len2to1, buf_size - len2to1);
+                len2to1 += readed;
+                if (readed == 0)
+                {
+                    fdp[1].deads[0] = true; 
+                    fdp[1].closed = true;
+                }
+            }
+            if ((!fdp[0].deads[1]) && (len1to2 > 0) && (fds[1].revents & POLLOUT))
+            {
+                written = write(fd2, buf1to2, len1to2);
+                memmove(buf1to2, buf1to2 + written, len1to2 - written);
+                len1to2 -= written;
+            }
+        if (fdp[0].deads[0] && len1to2 == 0)
+            fdp[0].deads[1] = true;
+        if (fdp[1].deads[0] && len2to1 == 0)
+            fdp[1].deads[1] = true;
+
+        if (!fdp[0].closed && len1to2 < buf_size)
+            fds[0].events |= POLLIN;
+        if (!fdp[1].closed && len2to1 < buf_size)
+            fds[1].events |= POLLIN;
+        if (len1to2 > 0 && !fdp[0].deads[1])
+            fds[1].events |= POLLOUT; 
+        if (len2to1 > 0 && !fdp[1].deads[1])
+            fds[0].events |= POLLOUT;
+        }
+    }
+    return;
+}
 
 int my_write(int fd, const char *str, int len)
 {
@@ -89,7 +195,7 @@ int main()
         while (true)
         {
             int cfd = accept(sfd, &address, &address_len); 
-            
+            perror("accepted");
             int pid = fork();
             if (pid)
             {
@@ -101,30 +207,16 @@ int main()
                 close(STDOUT);
                 close(STDERR);
                 close(sfd);
-                int master, slave;
                 char buf[4096];
+                int master, slave;
                 openpty(&master, &slave, buf, NULL, NULL);
                 if (fork())
                 {
                     close(slave);
-                    int buf_size = 4096;
-                    char buf[buf_size];
-                    int readed;
-                    while (true) 
-                    {
-                        readed = read(master, buf, buf_size);
-                        if (readed > 0) 
-                        {
-                            write(cfd, buf, readed);
-                        }
-                        readed = read(cfd, buf, buf_size);
-                        if (readed > 0) 
-                        {
-                            write(master, buf, readed);
-                        }
-                        sleep(2);
-                    }
+                    my_poll(master, cfd);
+                    close(master);
                     close(cfd);
+                    exit(1);
                 }
                 else
                 {
