43f94241f74679383758d4aa777e9903df158f9d
GodGefest <godgefest@gmail.com>
2013-05-31 12:01:35 +0400

    smth changed

--- rshell/rshell.c
+++ rshell/rshell.c
@@ -5,6 +5,8 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <pty.h>
 
 #define STDIN 0
 #define STDOUT 1
@@ -30,88 +32,135 @@ int my_write(int fd, const char *str, int len)
 
 int main()
 {
-
-    struct addrinfo hints; 
-    struct addrinfo *result;
-
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-    hints.ai_protocol = 0;
-    hints.ai_canonname = NULL;
-    hints.ai_addr = NULL;
-    hints.ai_next = NULL;
-
-    int res_getaddrinfo = getaddrinfo(NULL, "8822", &hints, &result);
-    if (res_getaddrinfo != 0)
+    int main_pid = fork();
+    if (main_pid)
     {
-        const char *error = gai_strerror(res_getaddrinfo);
-        my_write(STDERR, error, strlen(error)); 
-        exit(EXIT_FAILURE);
+        wait();
     }
-
-    int sfd;
-    struct addrinfo *it; 
-    for (it = result; it != 0; it = it->ai_next)
+    else
     {
-        sfd = socket(it->ai_family, it->ai_socktype, it->ai_protocol);
-        if (sfd == -1)
+        setsid();
+
+        struct addrinfo hints; 
+        struct addrinfo *result;
+
+        memset(&hints, 0, sizeof(struct addrinfo));
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_flags = AI_PASSIVE;
+        hints.ai_protocol = 0;
+        hints.ai_canonname = NULL;
+        hints.ai_addr = NULL;
+        hints.ai_next = NULL;
+
+        int res_getaddrinfo = getaddrinfo(NULL, "8822", &hints, &result);
+        if (res_getaddrinfo != 0)
         {
-            continue;
+            const char *error = gai_strerror(res_getaddrinfo);
+            my_write(STDERR, error, strlen(error)); 
+            exit(EXIT_FAILURE);
         }
 
-        int opt_val = 1;
-        setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof(int));
-        
-        if (bind(sfd, it->ai_addr, it->ai_addrlen) == 0)
+        int sfd;
+        struct addrinfo *it; 
+        for (it = result; it != 0; it = it->ai_next)
         {
-            break;
-        }
-        close(sfd);
-    } 
+            sfd = socket(it->ai_family, it->ai_socktype, it->ai_protocol);
+            if (sfd == -1)
+            {
+                continue;
+            }
 
-    if (it == NULL)
-    {
-        const char *error = "Coudln't bind\n";
-        my_write(STDERR, error, strlen(error));
-        exit(EXIT_FAILURE);
-    }
-
-    freeaddrinfo(result);
-    
-    int listen_res = listen(sfd, MAX_COUNT_OF_CONNECTIONS);
-    if (listen_res == -1) 
-    {
-        perror("Listen failed");
-    }
-
-    struct sockaddr address;
-    socklen_t address_len = sizeof(address);
-
-    while (true)
-    {
-        int cfd = accept(sfd, &address, &address_len); 
-        
-        int pid = fork();
-        if (pid)
-        {
-            close(cfd);
-        }
-        else 
-        {
-            dup2(cfd, STDIN);
-            dup2(cfd, STDOUT);
-            dup2(cfd, STDERR);
+            int opt_val = 1;
+            setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof(int));
+            
+            if (bind(sfd, it->ai_addr, it->ai_addrlen) == 0)
+            {
+                break;
+            }
             close(sfd);
-            const char *message = "Hello world!\n";
-            my_write(cfd, message, strlen(message));
-            close(cfd);
-            exit(0);
+        } 
+
+        if (it == NULL)
+        {
+            const char *error = "Coudln't bind\n";
+            my_write(STDERR, error, strlen(error));
+            exit(EXIT_FAILURE);
         }
 
-    }
+        freeaddrinfo(result);
+        
+        int listen_res = listen(sfd, MAX_COUNT_OF_CONNECTIONS);
+        if (listen_res == -1) 
+        {
+            perror("Listen failed");
+        }
 
-    close(sfd);
+        struct sockaddr address;
+        socklen_t address_len = sizeof(address);
+
+        while (true)
+        {
+            int cfd = accept(sfd, &address, &address_len); 
+            
+            int pid = fork();
+            if (pid)
+            {
+                close(cfd);
+            }
+            else 
+            {
+               // dup2(cfd, STDIN);
+               // dup2(cfd, STDOUT);
+               // dup2(cfd, STDERR);
+                close(STDIN);
+                close(STDOUT);
+                close(STDERR);
+                close(sfd);
+                int master, slave;
+                char buf[4096];
+                fcntl(master, F_SETFL, fcntl(master, F_GETFL) | O_NONBLOCK);
+                fcntl(cfd, F_SETFL, fcntl(cfd, F_GETFL) | O_NONBLOCK);
+                openpty(&master, &slave, buf, NULL, NULL);
+                if (fork())
+                {
+                    close(slave);
+                    int buf_size = 4096;
+                    char buf[buf_size];
+                    int readed;
+                    while (true) 
+                    {
+                        readed = read(master, buf, buf_size);
+                        if (readed > 0) 
+                        {
+                            write(cfd, buf, readed);
+                        }
+                        readed = read(cfd, buf, buf_size);
+                        if (readed > 0) 
+                        {
+                            write(master, buf, readed);
+                        }
+                        sleep(2);
+                    }
+                    close(cfd);
+                }
+                else
+                {
+                    setsid();
+                    close(master);
+                    dup2(slave, STDIN);
+                    dup2(slave, STDOUT);
+                    dup2(slave, STDERR);
+                    close(cfd);
+                    int fd = open(buf, O_RDWR);
+                    close(fd);
+                    execl("/bin/bash", "/bin/bash", NULL);
+                }
+            }
+
+        }
+
+        close(sfd);
+    }
     return 0;
 }
